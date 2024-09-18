9de0174f8254f778c73730115b7ef9ec156fe605
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-09-18 15:20:55 +0300

    animate/exploding

--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -1,11 +1,26 @@
 uniform float uSize;
 uniform vec2 uResolution;
 attribute float aSize;
+uniform float uProgress;
+
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
 
 void main()
-{
+{  
+    vec3 newPosition = position;
+
+   // Exploding
+      float explodingProgress = remap(uProgress, 0.0, 0.1, 0.0, 1.0);
+    explodingProgress = clamp(explodingProgress, 0.0, 1.0);
+     explodingProgress = 1.0 - pow(1.0-explodingProgress, 3.0);
+    newPosition = mix(vec3(0.0), newPosition, explodingProgress);
+
+
    // Final Position
- vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+ vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
  vec4 viewPosition = viewMatrix * modelPosition;
  gl_Position = projectionMatrix * viewPosition;
 
